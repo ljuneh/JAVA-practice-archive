23974914ca1183c2145178eb66ae2b0b06ae1782
GithubWoon <eo2458@naver.com>
2023-08-07 10:03:14 +0900

    8/07

--- day06/src/com/day06/HeapSort.java
+++ day06/src/com/day06/HeapSort.java
@@ -11,25 +11,25 @@ public class HeapSort {
 	public static void heapSort(int[] arr) {
 		int n = arr.length;
 
-	    // Build max heap
+	    
 	    for (int i = n / 2 - 1; i >= 0; i--) {
 	        heapify(arr, n, i);
 	    }
 
-	    // Extract elements from heap one by one
+	    
 	    for (int i = n - 1; i > 0; i--) {
 	        int temp = arr[0];
 	        arr[0] = arr[i];
 	        arr[i] = temp;
 
-	        // Call heapify on the reduced heap
+	        
 	        heapify(arr, i, 0);
 	    }
 	}
 	public static void heapify(int[] arr, int n, int i) {
 		int largest = i;
-	    int l = 2 * i + 1; // Left child
-	    int r = 2 * i + 2; // Right child
+	    int l = 2 * i + 1; 
+	    int r = 2 * i + 2; 
 
 	    if (l < n && arr[l] > arr[largest]) largest = l;
 	    if (r < n && arr[r] > arr[largest]) largest = r;
@@ -39,7 +39,7 @@ public class HeapSort {
 	        arr[i] = arr[largest];
 	        arr[largest] = swap;
 
-	        // Recursively heapify the affected sub-tree
+	        
 	        heapify(arr, n, largest);
 	    }
 	}
