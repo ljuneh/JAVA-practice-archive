60975c9905f3e86a97ee1ca5ec3a5bf25bfe982f
ljuneh <glglglr670@gmail.com>
2023-08-29 11:16:11 +0900

    8/29

--- homeShopping/src/com/main/StockAdder.java
+++
@@ -1,9 +0,0 @@
-package com.main;
-
-public class StockAdder {
-	
-	public static void addNewGoods(Goods goods, int number, GoodsListInfo goodsListInfo, StockInfo stockInfo) {
-		goodsListInfo.add(goods.getName(), goods);
-		stockInfo.register(goods.getName(), number);
-	}
-}
